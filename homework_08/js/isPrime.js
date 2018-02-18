cef0a171dc6dd6e3bb03c5df6b80435e119dceca
Mykola Periv <34752246+perivmv@users.noreply.github.com>
2018-02-18 09:30:00 +0200

    Add files via upload

---
+++ homework_08/js/isPrime.js
@@ -0,0 +1,10 @@
+function isPrime(input) {
+    var prime = true;
+    for (var i = 2; i <= Math.sqrt(input); i++) {
+        if (input % i == 0) {
+            prime = false;
+            break;
+        }
+    }
+    return prime && (input > 1);
+}
