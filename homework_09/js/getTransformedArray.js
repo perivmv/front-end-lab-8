6671fe9961de66c4d4ff305f81263b6970506807
Mykola Periv <34752246+perivmv@users.noreply.github.com>
2018-02-20 20:46:59 +0200

    Update getTransformedArray.js

--- homework_09/js/getTransformedArray.js
+++ homework_09/js/getTransformedArray.js
@@ -1,17 +1,3 @@
-/* 
-let a = [2, 4, 6, 8, 9, 15]
-
-function transform(a) {
-    let b = ['4', '16', '64']
-    a.forEach(function(e) {
-        b.push(e.toString());
-    });
-    return b;
-}
-
-transform(a) */
-
-
 function getTransformedArray(array, func) {
    return forEach(array, func);
 }
